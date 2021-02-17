96d92642c302ad5787589e3b9f2292a006432e51
kcinay055679 <ya.minder05@gmail.com>
2021-02-17 14:38:07 +0100

    Add project

---
+++ Semesterarbeit_Yanick_Minder/Assets/Scripts/ContMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float speed = 10;
+    float horizontalmovement = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalmovement = Input.GetAxisRaw("Horizontal") * speed;
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalmovement * Time.fixedDeltaTime, false, false);
+    }
+}
---
+++ Semesterarbeit_Yanick_Minder/Assets/Scripts/NormMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormMovement : MonoBehaviour
+{
+
+    public float speed = 1;
+    public float sprungkraft = 1;
+
+
+    float verticalmovement;
+    private Rigidbody2D rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalmovement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(verticalmovement, 0, 0) * speed * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rigid.velocity.y) < 0.001f)
+        {
+            rigid.AddForce(new Vector2(0, sprungkraft), ForceMode2D.Impulse);
+        }
+    }
+}
+
+
