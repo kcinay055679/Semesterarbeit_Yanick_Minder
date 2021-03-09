4110e52bfe32b52f430aef9762afc3369b9f2dca
kcinay055679 <ya.minder05@gmail.com>
2021-03-09 17:40:26 +0100

    Add smartphone control

--- Semesterarbeit_Yanick_Minder/Assets/Scripts/NormMovement.cs
+++ Semesterarbeit_Yanick_Minder/Assets/Scripts/NormMovement.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.UI;
+
 public class NormMovement : MonoBehaviour
 {
 
     public float speed = 1;
     public float sprungkraft = 1;
     public Joystick joystick;
+    public Button Jump;
 
     float verticalmovement;
     private Rigidbody2D rigid;
@@ -17,14 +20,20 @@ public class NormMovement : MonoBehaviour
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        //Jump = GetComponent<Button>();
+        Button btn = Jump.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+        //Jump.onClick.AddListener(TaskOnClick);
         Application.targetFrameRate = 60;
     }
-
+    
     // Update is called once per frame
     void Update()
     {
+
+        
         verticalmovement = Input.GetAxis("Horizontal");
-        verticalmovement = joystick.Horizontal;
+        //verticalmovement = joystick.Horizontal;
         transform.position += new Vector3(verticalmovement, 0, 0) * speed * Time.deltaTime;
 
         if (Input.GetButtonDown("Jump") && Mathf.Abs(rigid.velocity.y) < 0.001f)
@@ -32,6 +41,10 @@ public class NormMovement : MonoBehaviour
             rigid.AddForce(new Vector2(0, sprungkraft), ForceMode2D.Impulse);
         }
     }
+    public void TaskOnClick() {
+        rigid.AddForce(new Vector2(0, sprungkraft), ForceMode2D.Impulse);
+    }
+
 }
 
 
