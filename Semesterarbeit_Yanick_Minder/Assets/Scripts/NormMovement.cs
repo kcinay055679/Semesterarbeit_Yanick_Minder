d97944183becbd4f6e5c3afed279ceadceddae68
kcinay055679 <ya.minder05@gmail.com>
2021-03-09 15:33:32 +0100

    add Joystick

--- Semesterarbeit_Yanick_Minder/Assets/Scripts/NormMovement.cs
+++ Semesterarbeit_Yanick_Minder/Assets/Scripts/NormMovement.cs
@@ -7,21 +7,24 @@ public class NormMovement : MonoBehaviour
 
     public float speed = 1;
     public float sprungkraft = 1;
-
+    public Joystick joystick;
 
     float verticalmovement;
     private Rigidbody2D rigid;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
         verticalmovement = Input.GetAxis("Horizontal");
+        verticalmovement = joystick.Horizontal;
         transform.position += new Vector3(verticalmovement, 0, 0) * speed * Time.deltaTime;
 
         if (Input.GetButtonDown("Jump") && Mathf.Abs(rigid.velocity.y) < 0.001f)
